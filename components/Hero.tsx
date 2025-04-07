6e67d5679c723471564bf405598d0b6949f75f07
chuanzi <tianjiangchuanzi@163.com>
2025-04-07 21:49:28 +0800

    修复部署错误

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,5 @@
 'use client';
 
-import Image from 'next/image';
-import NavLink from './NavLink';
-
-let heroImages = ['/1.png', '/6.png', '/3.png', '/4.png', '/5.png', '/2.png'];
-
 export default function Hero() {
   return (
     <section className="pb-16">
